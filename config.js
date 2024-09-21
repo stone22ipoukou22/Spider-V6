b0bad00a91d4aa48e9c162a39b65d3e519d73804
stone22ipoukou22 <156923769+stone22ipoukou22@users.noreply.github.com>
2024-09-21 12:30:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,19 @@
 //Bot by 𝕶𝖎𝖓𝖌 𝕾𝖆𝖒//
-
-
 require("./all/module")
-global.owner = "254742491666" 
-global.namaCreator = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
+global.owner = "242066328691" 
+global.namaCreator = "DATA" 
 global.autoJoin = false 
-global.antilink = false 
+global.antilink = true 
 global.versisc = '6.0' 
 global.codeInvite = "https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e"
 global.isLink = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
 global.sessionName = 'session'
 global.thumb = fs.readFileSync("./thumb.png") 
 global.audionya = fs.readFileSync("./all/sound.mp3") 
-global.packname = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
-global.author = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
+global.packname = "DATA" 
+global.author = "DATA JOKER" 
 global.jumlah = "5" 
-global.namabot = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
+global.namabot = "DATA JOKER" 
 global.domain = 'https://kingsam.sellerpanelerol.me' // your domian
 global.apikey = 'ptla_VRxkhvalZQ3EhHyAax9QYxgT5cbsx2MrO69gBpVZmmd' // Isi Apikey Plta Lu
 global.capikey = 'ptlc_sQ3d4GxCiztyV5DbhUJSWtvnSCyKzeMXoWIr2lVyqal' // Isi Apikey Pltc Lu
